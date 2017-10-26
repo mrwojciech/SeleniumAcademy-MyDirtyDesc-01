aac88254792058ccf2165bddeb28aa00713c4f9d
Wojciech Sobasik <wojciech.sobasik@gmail.com>
2017-10-26 14:27:09 +0200

    poprawione waity, metody, logger przeniesiony do waitow, rozpoczete webelementy customowe, poprawione uruchamianie testow z kilku klas, driver null po quite, oraz wywalilem drivera z testow i dodalem przejscie na strone startowa zaraz po wejsciu do klasy testow przy tworzeniu drivera (tak by inicjalizujac elementy na stronie byly juz one widoczne)

--- src/main/java/com/s3group/BasePage/BasePage.java
+++ src/main/java/com/s3group/BasePage/BasePage.java
@@ -1,13 +1,13 @@
 package com.s3group.BasePage;
 
-import com.s3group.Utils.MyWaits;
+import com.s3group.Utils.Waits;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.s3group.Utils.MyDriver.getDriver;
+import static com.s3group.Utils.Driver.getDriver;
 
-public class BasePage extends MyWaits {
+public class BasePage extends Waits {
 
     public final Logger log = LoggerFactory.getLogger(this.getClass());
 
